bb9eeca0f1072a2d207257ec2e39bf98a16bc882
Elijah DeLee <kdelee@redhat.com>
2017-08-02 13:36:33 -0400

    fix bash syntax

--- ci/jjb/scripts/pulp-restore.sh
+++ ci/jjb/scripts/pulp-restore.sh
@@ -7,6 +7,6 @@ ansible-playbook --connection local -i hosts ci/ansible/pulp_restore.yaml \
     -e "pulp_version=${PULP_VERSION}" \
     -e "rhn_password=${RHN_PASSWORD}" \
     -e "rhn_pool=${RHN_POOL}" \
-    -e "rhn_username=${RHN_USERNAME}"
-    -e "local_bkup_dir=${WORKSPACE}/local_backups"
+    -e "rhn_username=${RHN_USERNAME}" \
+    -e "local_bkup_dir=${WORKSPACE}/local_backups" \
     -e "remote_bkup_dir=${WORKSPACE}/remote_backups"
